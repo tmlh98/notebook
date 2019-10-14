04633f878bfc40f681bf2534e39ff843a05d75c1
TianXin <tmlh98@aliyun.com>
2019-10-14 19:19:58 +0800

    2019-10-14

--- sh-commit.sh
+++ sh-commit.sh
@@ -1,8 +1,8 @@
 #!/bash/bin
 MSG=$1
 
-if [ ! -n "$MSG"]; then
-	MSG="."
+if [ ! -n "$MSG" ]; then
+	MSG=`date +"%Y-%m-%d"`
 fi
 
 git add .
@@ -10,7 +10,7 @@ git add .
 
 echo '----------(git add )---------'
 
-git commit -m "$MSG"
+git commit -m "${MSG}"
 
 
 echo '----------(git commit )---------'
